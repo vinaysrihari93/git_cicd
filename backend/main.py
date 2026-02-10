1b8df92bd3aaf5f1ca66877aded88f36a39860ab
vinaysrihari93 <vinaysrihari93@gmail.com>
2026-02-10 14:21:32 +0530

    commit 9

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ MODEL_PATH = Path(os.getenv("MODEL_PATH", "models/model.pkl"))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Or ["https://git-cicd-1-ngtw.onrender.com"]
+    allow_origins=["https://git-cicd-1-ngtw.onrender.com"],  # Or ["https://git-cicd-1-ngtw.onrender.com"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
