2963ca47053ec8a8cc35497e26d725889298faeb
vinaysrihari93 <vinaysrihari93@gmail.com>
2026-02-10 10:06:41 +0530

    commit 5

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,25 @@ from pydantic import BaseModel
 from pathlib import Path
 import pickle
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Linear Regression API")
 
 import os
 MODEL_PATH = Path(os.getenv("MODEL_PATH", "models/model.pkl"))
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://git-cicd-1-ngtw.onrender.com",  # ← your Streamlit URL
+        "http://localhost:8501",                 # for local testing
+        "*"                                      # temporary wildcard for quick test (remove later)
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # MODEL_PATH = r"D:\mlops_sample_cicid\backend\models\model.pkl" 
 try:
